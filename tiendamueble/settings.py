766066c6c9935c4d2297df9ae74bcf5d0e4820b0
Fanor1234 <luis_faurp@hotmail.com>
2023-04-30 17:43:47 -0400

    allowed

--- tiendamueble/settings.py
+++ tiendamueble/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3kndr4ys2vh5+)u1bjdb-6ez=$(vk%y1pesp##-!+060c12b=x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
